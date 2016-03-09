185c2c72e0aef4621c4319c7cf6ccd11b451fc43
Matt Layher <mdlayher@gmail.com>
2016-03-09 17:33:20 -0500

    *: initial commit, no tests

---
+++ README.md
@@ -0,0 +1,4 @@
+rtorrent [![GoDoc](http://godoc.org/github.com/mdlayher/rtorrent?status.svg)](http://godoc.org/github.com/mdlayher/rtorrent) [![Build Status](https://travis-ci.org/mdlayher/rtorrent.svg?branch=master)](https://travis-ci.org/mdlayher/rtorrent) [![Coverage Status](https://coveralls.io/repos/mdlayher/rtorrent/badge.svg?branch=master)](https://coveralls.io/r/mdlayher/rtorrent?branch=master) [![Report Card](http://goreportcard.com/badge/mdlayher/rtorrent)](http://goreportcard.com/report/mdlayher/rtorrent)
+========
+
+Package `rtorrent` implements a client for rTorrent.  MIT Licensed.
---
+++ rtorrent.go
@@ -0,0 +1,135 @@
+// Package rtorrent implements a client for rTorrent.
+package rtorrent
+
+import (
+	"net/http"
+
+	"github.com/kolo/xmlrpc"
+)
+
+// A Client is an rTorrent client.  It can be used to retrieve a
+// variety of statistics from rTorrent.
+type Client struct {
+	Downloads *DownloadService
+
+	xrc *xmlrpc.Client
+}
+
+// New creates a new Client using the input XML-RPC address and an optional
+// transport.  If transport is nil, a default one will be used.
+func New(addr string, transport http.RoundTripper) (*Client, error) {
+	xrc, err := xmlrpc.NewClient(addr, transport)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Client{
+		xrc: xrc,
+	}
+
+	c.Downloads = &DownloadService{c: c}
+
+	return c, nil
+}
+
+// Close frees a Client's resources.
+func (c *Client) Close() error {
+	return c.xrc.Close()
+}
+
+// DownloadTotal retrieves the total number of downloaded bytes since
+// rTorrent startup.
+func (c *Client) DownloadTotal() (int, error) {
+	return c.getInt("get_down_total")
+}
+
+// UploadTotal retrieves the total number of uploaded bytes since
+// rTorrent startup.
+func (c *Client) UploadTotal() (int, error) {
+	return c.getInt("get_up_total")
+}
+
+// DownloadRate retrieves the current download rate in bytes from rTorrent.
+func (c *Client) DownloadRate() (int, error) {
+	return c.getInt("get_down_rate")
+}
+
+// UploadRate retrieves the current upload rate in bytes from rTorrent.
+func (c *Client) UploadRate() (int, error) {
+	return c.getInt("get_up_rate")
+}
+
+const (
+	// downloadList is used in methods which retrieve a list of downloads.
+	downloadList = "download_list"
+)
+
+// A DownloadService is a wrapper for Client methods which operate on downloads.
+type DownloadService struct {
+	c *Client
+}
+
+// All retrieves a list of all downloads from rTorrent.
+func (s *DownloadService) All() ([]string, error) {
+	return s.c.getStringSlice(downloadList)
+}
+
+// Started retrieves a list of started downloads from rTorrent.
+func (s *DownloadService) Started() ([]string, error) {
+	return s.c.getStringSlice(downloadList, "started")
+}
+
+// Stopped retrieves a list of stopped downloads from rTorrent.
+func (s *DownloadService) Stopped() ([]string, error) {
+	return s.c.getStringSlice(downloadList, "stopped")
+}
+
+// Complete retrieves a list of complete downloads from rTorrent.
+func (s *DownloadService) Complete() ([]string, error) {
+	return s.c.getStringSlice(downloadList, "complete")
+}
+
+// Incomplete retrieves a list of incomplete downloads from rTorrent.
+func (s *DownloadService) Incomplete() ([]string, error) {
+	return s.c.getStringSlice(downloadList, "incomplete")
+}
+
+// Hashing retrieves a list of hashing downloads from rTorrent.
+func (s *DownloadService) Hashing() ([]string, error) {
+	return s.c.getStringSlice(downloadList, "hashing")
+}
+
+// Seeding retrieves a list of seeding downloads from rTorrent.
+func (s *DownloadService) Seeding() ([]string, error) {
+	return s.c.getStringSlice(downloadList, "seeding")
+}
+
+// Leeching retrieves a list of leeching downloads from rTorrent.
+func (s *DownloadService) Leeching() ([]string, error) {
+	return s.c.getStringSlice(downloadList, "leeching")
+}
+
+// Active retrieves a list of active downloads from rTorrent.
+func (s *DownloadService) Active() ([]string, error) {
+	return s.c.getStringSlice(downloadList, "active")
+}
+
+// getInt retrieves an integer value from the specified XML-RPC method.
+func (c *Client) getInt(method string) (int, error) {
+	var v int
+	err := c.xrc.Call(method, nil, &v)
+	return v, err
+}
+
+// getStringSlice retrieves a slice of string values from the specified
+// XML-RPC method.
+func (c *Client) getStringSlice(method string, args ...string) ([]string, error) {
+	send := []interface{}{""}
+	for _, a := range args {
+		send = append(send, a)
+	}
+
+	var v []string
+	err := c.xrc.Call(method, send, &v)
+	return v, err
+}
